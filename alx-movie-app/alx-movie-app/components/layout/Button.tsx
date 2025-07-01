7111fc182a5b17302b8f3e39aa50b3fc4552d2ce
dyphine anyanga <dyphineanyanga@gmail.com>
2025-07-01 06:56:44 +0300

    Setup project structure and add package.json with dependencies

---
+++ alx-movie-app/alx-movie-app/components/layout/Button.tsx
@@ -0,0 +1,10 @@
+const LayoutButton: React.FC = () => {
+  return (
+    <button className="px-4 py-2 bg-gray-800 text-white rounded">
+      Layout Button
+    </button>
+  );
+};
+
+export default LayoutButton;
+
